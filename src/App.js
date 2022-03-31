c3cbc1bb891f8a0eca1a2707d1b22e125b44b2ba
Irina Sabaliauskiene <sabaliauskieneirina@gmail.com>
2022-03-31 21:13:17 +0200

    before contacts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import AppBar from './components/AppBar/AppBar.js';
 import { lazy, Suspense } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import './App.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchCurrentUser } from './redux/auth/auth-operations';
 import authSelectors from './redux/auth/auth-selectors';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
+// import HomeView from './Views/HomeView';
 
 const RegisterView = lazy(() =>
   import('./Views/RegisterView/RegisterView.jsx')
@@ -14,40 +17,35 @@ const LoginView = lazy(() => import('./Views/LoginView/LoginView.jsx'));
 const ContactView = lazy(() => import('./Views/ContactView/ContactView.jsx'));
 
 export default function App() {
-  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
   const dispatch = useDispatch();
-
+  const isFetchingCurrentUser = useSelector(authSelectors.getIsFetchingCurrent);
   useEffect(() => {
     dispatch(fetchCurrentUser());
   }, [dispatch]);
 
   return (
-    <>
-      <AppBar />
-      <Suspense
-        fallback={<h1>Wait a second, look at the sky during loading...</h1>}
-      >
-        {!isLoggedIn ? (
+    !isFetchingCurrentUser && (
+      <>
+        <AppBar />
+        <Suspense
+          fallback={<h1>Wait a second, look at the sky during loading...</h1>}
+        >
           <Switch>
-            <Route exact path="/">
-              {/* <HomeView /> */}
-            </Route>
-            <Route path="/register">
+            <PublicRoute path="/register" restricted>
               <RegisterView />
-            </Route>
-            <Route path="/login">
+            </PublicRoute>
+            <PublicRoute path="/login" restricted>
               <LoginView />
-            </Route>
-          </Switch>
-        ) : (
-          <Switch>
-            <Route path="/contacts">
+            </PublicRoute>
+            <PublicRoute exact path="/">
+              {/* <HomeView /> */}
+            </PublicRoute>
+            <PrivateRoute path="/contacts">
               <ContactView />
-            </Route>
-            <Redirect to="/login" />
+            </PrivateRoute>
           </Switch>
-        )}
-      </Suspense>
-    </>
+        </Suspense>
+      </>
+    )
   );
 }
